f0116fdcd0cdab737223ebb81eaa81e328dc1822
Joe-Sendra <joe.sendra@gmail.com>
2020-02-22 19:51:34 -0600

    upgrade angular add ng-bootstrap🥾

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IssueEditComponent } from './issue/issue-edit/issue-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IssueEditComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/issue/issue-edit/issue-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-edit',
+  templateUrl: './issue-edit.component.html',
+  styleUrls: ['./issue-edit.component.css']
+})
+export class IssueEditComponent {
+
+}
